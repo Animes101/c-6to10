985fc8fee665f5df13a373597db954b9355f51f2
Animes <animesbarman101@gmail.com>
2025-10-30 11:39:52 +0600

    ok

--- index.cpp
+++ index.cpp
@@ -14,29 +14,45 @@ using namespace std;
 
 int main(){
 
-    int nam1, name2, total;
+//     int nam1, name2, total;
 
-    nam1=700;
-    name2=400;
+//     nam1=700;
+//     name2=400;
 
-    total= nam1+ name2;
+//     total= nam1+ name2;
 
-    cout<< "total "<< total;
-
-
-    int a;
-    double b;
-    char ch;
-
-    a=4000;
-    b=666.444;
-    ch='a';
-
-    cout<< a <<b << ch;
+//     cout<< "total "<< total;
 
 
-   char name[14]="animes barman";
+//     int a;
+//     double b;
+//     char ch;
 
-   cout<<name;
+//     a=4000;
+//     b=666.444;
+//     ch='a';
+
+//     cout<< a <<b << ch;
+
+
+//    char name[14]="animes barman";
+
+//    cout<<name;
+
+
+       int number;
+       int number2;
+       int total;
+
+       cin>> number;
+       cin>> number2;
+
+       total =number + number2;
+
+       cout<< total;
+
+
+
+
 
 }
