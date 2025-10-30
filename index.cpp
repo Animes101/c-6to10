b5dba6a2bbe614f15c13effc5dd6baa215a38ad1
Animes <animesbarman101@gmail.com>
2025-10-30 11:33:33 +0600

    add string

--- index.cpp
+++ index.cpp
@@ -1,11 +1,42 @@
 #include <iostream>>
 using namespace std;
 
-//comment 
+// //comment 
 
-int main()
-{
+// int main()
+// {
 
-    cout<< "hello c++";
-    cin.get();
+//     cout<< "hello c++";
+//     cin.get();
+// }
+
+//keyword variable , data type
+
+int main(){
+
+    int nam1, name2, total;
+
+    nam1=700;
+    name2=400;
+
+    total= nam1+ name2;
+
+    cout<< "total "<< total;
+
+
+    int a;
+    double b;
+    char ch;
+
+    a=4000;
+    b=666.444;
+    ch='a';
+
+    cout<< a <<b << ch;
+
+
+   char name[14]="animes barman";
+
+   cout<<name;
+
 }
